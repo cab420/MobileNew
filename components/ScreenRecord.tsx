29c147fba25f9b07522b9d711614e30dbc8f3ced
cab420 <alex.m.dwyer@gmail.com>
2023-05-10 21:31:59 +1000

    more changes

--- components/ScreenRecord.tsx
+++ components/ScreenRecord.tsx
@@ -1,6 +1,8 @@
 import RecordScreen, { RecordingResult } from 'react-native-record-screen'; //importing this for screen RECORDING not sharing
 import React, { useState, useMemo, useCallback } from 'react';
 import Video from 'react-native-video';
+//import RNFS from 'react-native-fs';
+
 import {
   StyleSheet,
   View,
@@ -22,24 +24,26 @@ export default function ScreenRecord() {
   const [recording, setRecording] = useState<boolean>(false);
   const [url, setUrl] = useState<string>('');
 
-  const _handleOnRecording = async () => {
+  const _handlestop = async () => {
     if (recording) {
       setRecording(false);
       const res = await RecordScreen.stopRecording().catch((error: any) =>
         console.warn(error)
       );
-      console.log('res', res);
       if (res?.status === 'success') {
-        setUrl(res.result.outputURL)
+        console.log("res", res);
         setUri(res.result.outputURL);
-      }
-    } else {
-      setUri('');
+        console.log("res result output", res.result.outputURL);
+        //console.log(RecordingResult);
+        
+      }}
+    }
+
+    const _handlestart = async () => {
       setRecording(true);
-      const res = await RecordScreen.startRecording({ mic: false, fps: 30, bitrate: 1024000 }).catch((error: any) => {
+      const res = await RecordScreen.startRecording({ mic: false, fps: 30, bitrate: 236390400 }).catch((error: any) => {
         console.warn(error);
         setRecording(false);
-        setUri('');
       });
 
       if (res === RecordingResult.PermissionError) {
@@ -48,16 +52,44 @@ export default function ScreenRecord() {
         setUri('');
       }
     }
-  };
+    
+    const btnStyle = useMemo(() => {
+      return recording ? styles.btnActive : styles.btnDefault;
+    }, [recording]);
 
-  const _handleOnCleanSandbox = useCallback(() => {
-    RecordScreen.clean();
-    setUri('');
-  }, []);
+  // const rnfsoutput = (() => {
+  //   RNFS.readDir(RNFS.DocumentDirectoryPath) // On Android, use "RNFS.DocumentDirectoryPath" (MainBundlePath is not defined)
+  //   .then((result2: any) => {
+  //     console.log('GOT RESULT', result2);
+  
+  //     // stat the first file
+  //     return Promise.all([RNFS.stat(result2[0].path), result2[0].path]);
+  //   })
+  //   .then((statResult: any) => {
+  //     if (statResult[0].isFile()) {
+  //       // if we have a file, read it
+  //       return RNFS.readFile(statResult[1], 'utf8');
+  //     }
+  
+  //     return 'no file';
+  //   })
+  //   .then((contents: any) => {
+  //     // log the file contents
+  //     console.log(contents);
+  //   })
+  //   .catch((err: any) => {
+  //     console.log(err.message, err.code);
+  //   });
+  // })
 
-  const btnStyle = useMemo(() => {
-    return recording ? styles.btnActive : styles.btnDefault;
-  }, [recording]);
+  // const _handleOnCleanSandbox = useCallback(() => {
+  //   RecordScreen.clean();
+  //   setUri('');
+  // }, []);
+
+  // const btnStyle = useMemo(() => {
+  //   return recording ? styles.btnActive : styles.btnDefault;
+  // }, [recording]);
 
   return (
     <>
@@ -70,7 +102,7 @@ export default function ScreenRecord() {
           </View>
         ) : (
           <View>
-            <Button onPress={_handleOnCleanSandbox} title="Clean Sandbox" />
+            
           </View>
         )}
       </View>
@@ -78,71 +110,17 @@ export default function ScreenRecord() {
         <ScrollView>
           <View style={styles.scrollView}>
             <Text style={styles.heading}>Lorem ipsum dolor sit amet</Text>
-            <Text style={styles.text}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-              malesuada enim id fermentum pretium. Sed tempor, urna sed
-              facilisis convallis, massa eros porttitor lectus, ac maximus urna
-              mauris et lorem. In ac pretium felis, quis iaculis odio.
-              Vestibulum et arcu in leo egestas maximus eu a risus. Praesent nec
-              viverra mauris, at porta ligula. Sed at hendrerit dolor. Mauris
-              quis scelerisque mi. Nam porttitor justo molestie orci hendrerit,
-              quis rutrum dui finibus. Cras tincidunt libero non nulla
-              malesuada, a fermentum mi posuere. Nunc ultricies consectetur
-              lectus, vel ullamcorper ipsum ornare sed. Etiam rhoncus nunc ac
-              est efficitur consectetur.
-            </Text>
-            <Text style={styles.heading}>Sed vitae semper dolor</Text>
-            <Text style={styles.text}>
-              Sed vitae semper dolor. Nulla at blandit neque. Cras luctus
-              ullamcorper nisi, at venenatis purus porta eu. Praesent sed ante
-              sed orci placerat efficitur. Maecenas sed dapibus enim.
-              Suspendisse ac imperdiet mauris. Pellentesque arcu justo, accumsan
-              eu turpis vel, ornare dapibus urna. Fusce vitae sagittis urna. Sed
-              pulvinar justo ipsum, at porta tortor aliquet vitae. Sed suscipit
-              orci suscipit pretium efficitur. In vulputate dictum quam,
-              lobortis euismod elit scelerisque sed. Donec sed nunc justo.
-              Pellentesque habitant morbi tristique senectus et netus et
-              malesuada fames ac turpis egestas.
-            </Text>
-            <Text style={styles.heading}>Nam ligula nisi</Text>
-            <Text style={styles.text}>
-              Nam ligula nisi, tempus et condimentum vel, tincidunt in diam.
-              Vestibulum lorem sapien, gravida id semper non, interdum at risus.
-              Praesent viverra posuere aliquet. Cras convallis vitae augue sed
-              accumsan. Aenean vel tincidunt orci, scelerisque dapibus sem. Sed
-              porta neque sit amet tincidunt imperdiet. Phasellus tortor lacus,
-              mattis id placerat a, blandit non neque. Morbi maximus, erat eu
-              tincidunt interdum, ipsum neque maximus nisi, at viverra nunc
-              purus volutpat nibh. Curabitur sagittis ac metus ac condimentum.
-              Integer sodales erat quis turpis ornare imperdiet. Aenean
-              hendrerit fringilla ipsum, eget egestas lectus vestibulum quis. In
-              auctor, dolor quis mattis tincidunt, lacus leo interdum mi, a
-              viverra quam libero non nisl.
-            </Text>
-            <Text style={styles.heading}>Etiam non ante augue</Text>
-            <Text style={styles.text}>
-              Etiam non ante augue. Integer varius mi eget nulla rutrum commodo.
-              Suspendisse in congue metus. Curabitur ullamcorper quam eu mollis
-              commodo. Vivamus dictum velit tortor, quis blandit ante euismod
-              vel. Cras placerat, eros at gravida volutpat, enim mi fermentum
-              ligula, et suscipit tortor ipsum eget ante. Nullam viverra
-              dignissim turpis, in consequat velit aliquam ut.
-            </Text>
-            <Text style={styles.heading}>Praesent sed orci turpis</Text>
-            <Text style={styles.text}>
-              Praesent sed orci turpis. Aenean gravida quam quis sollicitudin
-              rutrum. Nullam ac risus sem. Aenean erat arcu, cursus sed nibh
-              vel, venenatis sagittis odio. Mauris volutpat lectus enim, a
-              interdum felis eleifend at. In porta sollicitudin fringilla. Nam
-              eu mauris nisi. Donec vitae lobortis nulla. Pellentesque euismod
-              tristique mauris, non eleifend lacus laoreet a.
-            </Text>
+            
           </View>
         </ScrollView>
       </SafeAreaView>
+
       <View style={styles.btnContainer}>
         <TouchableHighlight onPress={
-          () => {_handleOnRecording(); console.log(url) }
+          () => {
+            _handlestart() 
+            //rnfsoutput()
+          }
          
           
           }>
@@ -151,6 +129,22 @@ export default function ScreenRecord() {
           </View>
         </TouchableHighlight>
       </View>
+
+      <View style={styles.btnContainer}>
+        <TouchableHighlight onPress={
+          () => {
+            _handlestop() 
+            //rnfsoutput()
+          }
+         
+          
+          }>
+          <View style={styles.btnWrapper}>
+            <View style={btnStyle} />
+          </View>
+        </TouchableHighlight>
+      </View>
+
       {uri ? (
         <View style={styles.preview}>
           <Video
@@ -162,8 +156,8 @@ export default function ScreenRecord() {
         </View>
       ) : null}
     </>
-  );
-}
+  )};
+
 
 const styles = StyleSheet.create({
   navbar: {
