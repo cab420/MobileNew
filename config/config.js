00ad957b617d701054c9772e303d528883222230
cab420 <alex.m.dwyer@gmail.com>
2023-05-26 21:59:06 +1000

    deployment test

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 //Update the URL variable to match that of the local IP address of the server
-const URL = "https://192.168.1.101"
+const URL = "http://192.168.10.159"
 //Leave below ports as as API = ":3002" and SOCKET = ":8000"
-const API_PORT = "https://192.168.1.101:3002";
+const API_PORT = "http://192.168.10.159:3002";
 const SOCKET_PORT = ":8000"
 //URL +
 
