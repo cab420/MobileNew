2a3cfadc95f4959c18988bc540d7efc55c75bac3
cab420 <alex.m.dwyer@gmail.com>
2023-05-27 11:54:51 +1000

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 //Update the URL variable to match that of the local IP address of the server
-const URL = "http://192.168.1.101"
+const URL = "http://192.168.10.159"
 //Leave below ports as as API = ":3002" and SOCKET = ":8000"
 const API_PORT = ":3002";
 const SOCKET_PORT = ":8000"
